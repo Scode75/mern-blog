e08c18d44a24f01a52a6b361b6deb1a4e46c9cad
Steve Ezeala <120297848+Scode75@users.noreply.github.com>
2023-08-01 15:09:03 +0100

    Updated Post.js

--- src/Post.js
+++ src/Post.js
@@ -7,7 +7,7 @@ function Post({_id,title,summary,cover,content,createdAt,author}) {
     <div className="post">
     <div className="image">
       <Link to={`/post/${_id}`}>
-        <img src={'http://localhost:5000/'+cover} alt="" />
+        <img src={'https://api-service-afvl.onrender.com/'+cover} alt="" />
       </Link>
     </div>
     <div className="texts">
