be6738d73155dfe48d308bccc5a0feaf7acbdddc
Steve Ezeala <120297848+Scode75@users.noreply.github.com>
2023-08-01 15:29:34 +0100

    Updated LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -10,7 +10,7 @@ function LoginPage() {
 
   async function login(ev){
     ev.preventDefault();
-    const response = await fetch('http://localhost:5000/login',{
+    const response = await fetch('https://api-service-afvl.onrender.com/login',{
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
